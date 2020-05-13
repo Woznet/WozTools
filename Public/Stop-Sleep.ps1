4093bb3486924096f2a4ca16ab11d0bc7aa0453f
Woz <Woznet@users.noreply.github.com>
2020-05-13 15:54:24 -0400

    Update Stop-Sleep.ps1

--- Public/Stop-Sleep.ps1
+++ Public/Stop-Sleep.ps1
@@ -1,12 +1,22 @@
 ﻿function Stop-Sleep {
   [CmdletBinding()]
-  [Alias('Awake')]
-  param()
+  #[Alias('')]
+  param(
+    [Parameter(Mandatory)]
+    [ValidateScript({
+          if(!(Test-Path -Path $_ -Include 'Caffeinated.exe')){
+            throw 'Set Caffeinated.exe Path'
+          } ; return $true
+    })]
+    [string]$Caffeinated = 'V:\tools\Caffeinated.exe'
+    
+  )
   if (Get-Process -Name Caffeinated -ea SilentlyContinue){
     Write-Verbose -Message 'Closing current Caffeinated process' -Verbose:$VerbosePreference
     Get-Process -Name Caffeinated | Stop-Process
-    Start-Sleep -Milliseconds 250
+    Start-Sleep -Milliseconds 350
+    Get-Process -Name explorer | Stop-Process -Force -Confirm:$false
   }
   Write-Verbose -Message 'Starting Caffeinated' -Verbose:$VerbosePreference
-  & V:\tools\Caffeinated.exe
+  & $Caffeinated
 }
