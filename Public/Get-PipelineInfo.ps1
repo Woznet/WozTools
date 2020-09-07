c536b2cf4378e5bc7f7b0a60894230fc22a6077f
Woz <Woznet@users.noreply.github.com>
2020-09-07 15:59:36 -0400

    Update Get-PipelineInfo.ps1

--- Public/Get-PipelineInfo.ps1
+++ Public/Get-PipelineInfo.ps1
@@ -1,12 +1,11 @@
-﻿Function Get-PipelineInfo {
-	[CmdletBinding()]
-	Param (
+function Get-PipelineInfo {
+	param (
 		[Parameter(Mandatory)]
 		[String]$Cmdlet
 	)
 	Write-Verbose -Message ('Pipeline informaiton for {0}.' -f $Cmdlet)
 	(Get-Help -Name $Cmdlet).Parameters.Parameter |	Where-Object PipelineInput -ne 'False' |
-	Select-Object -Property Name , @{N = 'ByValue' ;	E = { If ($_.PipelineInput -Like '*ByValue*') {$True}
-	Else {$False} }},	@{N = 'ByPropertyName' ; E = { If ($_.PipelineInput -Like '*ByPropertyName*') {$True}
-	Else {$False} }},	@{N = 'Type' ; E = {$_.Type.Name}}
+	Select-Object -Property Name, @{N = 'ByValue' ; E = { if ($_.PipelineInput -Like '*ByValue*') {$True}
+	else {$False} }}, @{N = 'ByPropertyName' ; E = { if ($_.PipelineInput -Like '*ByPropertyName*') {$True}
+	else {$False} }}, @{N = 'Type' ; E = {$_.Type.Name}}
 }
