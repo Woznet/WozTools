fb87c3f2312d3e01905d6ae4b07e2e8049ecab24
Woz <Woznet@users.noreply.github.com>
2020-09-07 16:00:10 -0400

    Update Invoke-InDirectory.ps1

--- Public/Invoke-InDirectory.ps1
+++ Public/Invoke-InDirectory.ps1
@@ -1,6 +1,5 @@
-﻿function Invoke-InDirectory {
+function Invoke-InDirectory {
   # https://gist.github.com/chriskuech/a32f86ad2609719598b073293d09ca03#file-tryfinally-2-ps1
-  [CmdletBinding()]
   Param(
     [Parameter(Mandatory)]
     [ValidateScript({
