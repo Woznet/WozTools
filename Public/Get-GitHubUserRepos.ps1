0bb545280d984d7bc952515409ca0b19aa8b6301
Woz <Woznet@users.noreply.github.com>
2020-10-07 18:52:57 -0400

    Update Get-GitHubUserRepos.ps1

--- Public/Get-GitHubUserRepos.ps1
+++ Public/Get-GitHubUserRepos.ps1
@@ -4,22 +4,17 @@
       .Synopsis
       Download GitHub User Gists & Repositories
       .DESCRIPTION
-      Requires pwsh version 7+
       Requires Module - PowerShellForGitHub
+      Requires Module - PForEach
       Requires git.exe
       .EXAMPLE
       Get-GitHubUserRepos -UserName WozNet -Path 'V:\git\users'
       .EXAMPLE
       'WozNet','PowerShell','Microsoft' | Get-GitHubUserRepos -Path 'V:\git\users'
-      .NOTES
-      Requires Module - PowerShellForGitHub
-      Requires pwsh version 7+
   #>
-  [CmdletBinding()]
   [Alias('dlgit')]
-  Param
-  (
-    # Param1 help - GitHub Username
+  Param (
+    # Param1 help - GitHub Usernames
     [Parameter(Mandatory,
         ValueFromPipeline
     )]
@@ -28,30 +23,20 @@
     [String[]]$UserName,
 
     # Param2 help - Directory to save Repositories
-    [Parameter(Mandatory)]
     [ValidateScript({
           Test-Path -Path $_ -PathType Container
     })]
-    [String]$Path
+    [String]$Path = 'V:\git\users',
+    
+    # Param3 help - ThrottleLimit for Invoke-ForEachParallel
+    [int]$ThrottleLimit = 15
   )
   Begin
   {
-    <#
-        if ($PSVersionTable.PSVersion -le '7.0') {
-        if (Get-Command -Name pwsh.exe) {
-        $cmd = $MyInvocation.Line
-        pwsh -NoProfile -Command ('& {0}' -f $cmd)
-        return
-        }
-        else {
-        throw 'Use PowerShell Core 7+'
-        }
-        }
-        if ($PSVersionTable.PSVersion -le '7.0') { throw 'Use PowerShell Core 7+' }
-    #>
     if (-not (Get-Command -Name git.exe)){ throw 'git.exe is missing' }
-    if (-not (Get-Module -ListAvailable -Name PowerShellForGitHub)) {throw 'PowerShellForGitHub - is not installed'}
-    Import-Module -Name PowerShellForGitHub -Force
+    if (-not (Get-Module -ListAvailable -Name PowerShellForGitHub)) {throw 'Install Module - PowerShellForGitHub'}
+    if (-not (Get-Module -ListAvailable -Name PForEach)){ throw 'Install Module - PForEach' }
+    Import-Module -Global -Name PowerShellForGitHub,PForEach
     $StopWatch = [System.Diagnostics.Stopwatch]::New()
     $html = @'
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
@@ -84,37 +69,65 @@ $.getJSON('https://api.github.com/users/' + username + '/gists', function (data)
 
   Process
   {
-    foreach ($GUser in $UserName)
+    foreach ($GitUser in $UserName)
+	{
+      $UserPath = Join-Path -Path $Path -ChildPath $GitUser
+      if (Test-Path -Path $UserPath -PathType Container)
+	  {
+        Get-GitHubRepository -OwnerName $GitUser | Sort-Object -Property updated_at -Descending |  ForEach-Object -Begin {$a = @()} -Process {
+          $a += [pscustomobject]@{
+            'Name' = $_.Name
+            'Git Updated' = $_.updated_at.ToShortDateString()
+            'Local Updated' = if ( Test-Path -Path (Join-Path -Path $UserPath -ChildPath $_.Name)) {
+              (Get-ChildItem -Path $UserPath -Filter $_.Name -Directory | Select-Object -ExpandProperty LastWriteTime).ToShortDateString()
+            }
+            else { Write-Output -InputObject 'N/A' }
+          }
+        }
+        $a | Out-GridView -PassThru | Select-Object -ExpandProperty Name | ForEach-Object {
+          $p1 = Join-Path -Path $UserPath -ChildPath $_ -Resolve -ErrorAction SilentlyContinue
+          if ($p1)
+		  {
+            Write-Output -InputObject ('Deleting: {0}' -f $p1)
+            Remove-Item -Path $p1 -Recurse -Force
+          }
+          $p1 = $null
+        }
+      }
+      else
+	  {
+        Write-Output -InputObject ('{0} - has not been cloned yet' -f $GitUser)
+      }
+    }
+
+    foreach ($GitUser in $UserName)
     {
-      $user = $GUser
-      $userdir = Join-Path -Path $Path -ChildPath $user
-      if (-not (Test-Path -Path $userdir))
+      $UserPath = Join-Path -Path $Path -ChildPath $GitUser
+      if (-not (Test-Path -Path $UserPath))
       {
-        New-Item -Path $userdir -ItemType Directory
+        New-Item -Path $UserPath -ItemType Directory
       }
       
-      $UserGist = Get-GitHubGist -UserName $user
+      $UserGist = Get-GitHubGist -UserName $GitUser
       if($UserGist)
       {
-        $gistdir = Join-Path -Path $userdir -ChildPath '_gist'
+        $gistdir = Join-Path -Path $UserPath -ChildPath '_gist'
         if (-not (Test-Path -Path $gistdir))
         {
           New-Item -Path $gistdir -ItemType Directory
         }
-        New-Item -Path $userdir -Name '_gist.html' -ItemType File -Value ($html.Replace('-----',$user)) -Force
-        Get-GitHubGist -UserName $user  | ForEach-Object {
-          # $gistdir = Join-Path -Path $userdir -ChildPath '_gist'
-          Start-Process -WorkingDirectory $gistdir -FilePath git.exe -ArgumentList ('clone --recursive {0}' -f ($PSItem.git_pull_url)) -WindowStyle Hidden -Wait
-        }
+        New-Item -Path $UserPath -Name '_gist.html' -ItemType File -Value ($html.Replace('-----',$GitUser)) -Force
+        $UserGist.git_pull_url | Invoke-ForEachParallel -Process {
+          Start-Process -WorkingDirectory $gistdir -FilePath git.exe -ArgumentList ('clone --recursive {0}' -f $PSItem) -WindowStyle Hidden -Wait
+        } -ThrottleLimit $ThrottleLimit
       }
-
-      $UserRepo = Get-GitHubRepository -OwnerName $user
-      '{0}{1}s Repositories' -f $user,("'")
+      
+      $UserRepo = Get-GitHubRepository -OwnerName $GitUser
+      '{0}{1}s Repositories' -f $GitUser,("'")
       $UserRepo | Format-Wide -Column 4
-      $UserRepo.clone_url | ForEach-Object {
-        # $userdir = Join-Path -Path $Path -ChildPath $user
-        Start-Process -WorkingDirectory $userdir -FilePath git.exe -ArgumentList ('clone --recursive {0}' -f ($PSItem)) -WindowStyle Hidden -Wait
-      }
+      $UserRepo.clone_url | Invoke-ForEachParallel -Process {
+        Start-Process -WorkingDirectory $UserPath -FilePath git.exe -ArgumentList ('clone --recursive {0}' -f $PSItem) -WindowStyle Hidden -Wait
+      } -ThrottleLimit $ThrottleLimit
     }
   }
 
