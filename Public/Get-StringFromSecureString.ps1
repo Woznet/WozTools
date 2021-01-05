2cc61ce3b90a4714d154937da9b819ef5e9082c2
WozNet <walker.chris87@gmail.com>
2021-01-05 15:55:57 -0500

    1.38

---
+++ Public/Get-StringFromSecureString.ps1
@@ -0,0 +1,39 @@
+﻿function Get-StringFromSecureString
+{
+  <#
+      .Synopsis
+      Decrypt SecureString
+      .DESCRIPTION
+      Get unecrypted string from a secure string
+      .EXAMPLE
+
+      $test = Get-Credential # Username - Tester, Password test1
+      $test.Password | Get-StringFromSecureString
+      .EXAMPLE
+
+      Get-StringFromSecureString -SecureString $test.Password
+      .INPUTS
+      securestring
+      .OUTPUTS
+      string
+  #>
+  [CmdletBinding(
+      PositionalBinding=$true
+  )]
+  [Alias('Convert-SecureString')]
+  [OutputType([String])]
+  Param (
+    [Parameter(
+        Mandatory,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName
+    )]
+    [Alias('Password')]
+    [ValidateNotNullOrEmpty()]
+    [securestring]$SecureString
+  )
+  Process
+  {
+    [Microsoft.PowerShell.DesiredStateConfiguration.Internal.DscClassCache]::GetStringFromSecureString($SecureString)
+  }
+}
