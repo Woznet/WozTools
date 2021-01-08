ece407a189b52194a877c3eff2c07317be96e8b7
WozNet <walker.chris87@gmail.com>
2021-01-08 16:15:03 -0500

    1.39

--- Public/Set-MaxPartitionSize.ps1
+++ Public/Set-MaxPartitionSize.ps1
@@ -4,25 +4,24 @@ function Set-MaxPartitionSize {
   param()
   dynamicparam {
     $ParamName = 'DriveLetter'
-    [String[]]$Values = (Get-WmiObject -Class win32_volume | ForEach-Object -MemberName DriveLetter | Sort-Object ).Replace(':', '')
+    [String[]]$Values = (Get-CimInstance -ClassName Win32_Volume | Select-Object -ExpandProperty DriveLetter | Sort-Object).Replace(':', '')
 
-    $Bucket = [Management.Automation.RuntimeDefinedParameterDictionary]::new()
-    $AttributeList = [Collections.ObjectModel.Collection[System.Attribute]]::new()
+    $Bucket = [System.Management.Automation.RuntimeDefinedParameterDictionary]::new()
+    $AttributeList = [System.Collections.ObjectModel.Collection[System.Attribute]]::new()
     $AttribValidateSet = [ValidateSet]::new($Values)
     $AttributeList.Add($AttribValidateSet)
     $AttribParameter = [Parameter]::new()
     $AttribParameter.Mandatory = $true
     $AttributeList.Add($AttribParameter)
-    $Parameter = [Management.Automation.RuntimeDefinedParameter]::new($ParamName, [String], $AttributeList)
+    $Parameter = [System.Management.Automation.RuntimeDefinedParameter]::new($ParamName, [String], $AttributeList)
     $Bucket.Add($ParamName, $Parameter)
     $Bucket
   }
-  begin{
-    $Letter = $PSBoundParameters[$ParamName]
-  }
   process{
+    $Letter = $PSBoundParameters[$ParamName]
+
     $Size = (Get-PartitionSupportedSize -DriveLetter $Letter).SizeMax
-    Resize-Partition -DriveLetter $Letter -Size $Size -PassThru -Verbose:$VerbosePreference
+    Resize-Partition -DriveLetter $Letter -Size $Size -Verbose:$VerbosePreference
+    Get-Volume -DriveLetter $Letter
   }
-  end{}
 }
