b98ad8f60492f55cfd5aa94d56576ae1ae358858
WozNet <walker.chris87@gmail.com>
2021-01-27 09:00:41 -0500

    v1.40

---
+++ Public/Set-ConsoleSize.ps1
@@ -0,0 +1,10 @@
+function Set-ConsoleSize {
+  [Alias('FixCon')]
+  param(
+    [int]$Width = 145,
+    [int]$Height = 45
+  )
+  if ($env:WT_SESSION) {throw 'Does Not Work in Microsoft Terminal'}
+  if ($Host.Name -notmatch 'ConsoleHost') {break}
+  [console]::SetWindowSize($Width,$Height)
+}
