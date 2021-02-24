20d921d894acc5f66dfb906b51724db7eb398860
WozNet <walker.chris87@gmail.com>
2021-02-23 20:13:20 -0500

    v1.40

--- Public/Invoke-InDirectory.ps1
+++ Public/Invoke-InDirectory.ps1
@@ -1,4 +1,4 @@
-function Invoke-InDirectory {
+﻿function Invoke-InDirectory {
   # https://gist.github.com/chriskuech/a32f86ad2609719598b073293d09ca03#file-tryfinally-2-ps1
   Param(
     [Parameter(Mandatory)]
