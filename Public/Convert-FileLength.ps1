d92b64b61fe264dffc5916bb5d0d567609ca4345
Woz <walker.chris87@gmail.com>
2022-10-22 23:30:38 -0400

    Update Convert-FileLength.ps1

--- Public/Convert-FileLength.ps1
+++ Public/Convert-FileLength.ps1
@@ -3,16 +3,17 @@ function Convert-FileLength {
 	[Alias('Convert-Size')]
   param(
     [Parameter(Mandatory,ValueFromPipelineByPropertyName,ValueFromPipeline)]
-    [Alias('Length')]
-    [long]$Size
+    [Alias('Size')]
+    [long]$Length
   )
   begin {
-<#
+    # TODO: Change "SizeConverter" to "LengthConverter"
 	try {
-$null = [WozDev.Win32API.SizeConverter]
-}
-catch {
-      $Signature =  @'
+    $null = [WozDev.Win32API.SizeConverter]
+  }
+  catch {
+    Write-Verbose ' ~ Required Type not loaded, starting Add-Type process ~' -Verbose
+    $MemberDef =  @'
 [DllImport("Shlwapi.dll", CharSet = CharSet.Auto)]
 public static extern long StrFormatByteSize(
 long fileSize,
@@ -20,24 +21,24 @@ System.Text.StringBuilder buffer,
 int bufferSize
 );
 '@
-      $SizeConverter = Add-Type -Name SizeConverter -Namespace 'WozDev.Win32API' -MemberDefinition $Signature -PassThru
-}
-#>
+    $SizeConverter = Add-Type -Name SizeConverter -Namespace 'WozDev.Win32API' -MemberDefinition $MemberDef
+  }
+
   }
   process {
 	  if ([WozDev.Win32API.SizeConverter] -as [type]) {
-	  $StringBuilder = [System.Text.StringBuilder]::new(1024)
-    $null = [WozDev.Win32API.SizeConverter]::StrFormatByteSize(
-	$Size,
-	$StringBuilder,
-	$StringBuilder.Capacity
-	)
-    return $StringBuilder.ToString()
+	    $StringBuilder = [System.Text.StringBuilder]::new(1024)
+      $null = [WozDev.Win32API.SizeConverter]::StrFormatByteSize(
+	      $Length,
+	      $StringBuilder,
+	      $StringBuilder.Capacity
+	    )
+      return $StringBuilder.ToString()
 	  }
 	  else {
 		  # Add ANSI color for missing SizeConverter
-		  return $Size
+		  return $Length
 	  }
-
   }
 }
+
