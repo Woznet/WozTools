21b0814d360a33fa4487e30863f619885fd801b6
Woz <walker.chris87@gmail.com>
2022-11-12 12:37:46 -0500

    11-12-22

--- Public/Enable-FileSizeFormat.ps1
+++ Public/Enable-FileSizeFormat.ps1
@@ -1,8 +1,9 @@
 function Enable-FileSize {
   [CmdletBinding()]
   param()
-  Update-FormatData -PrependPath ([System.IO.Path]::Combine((Split-Path -Path $PSScriptRoot -Parent),'Lib\FileLength-Formatting\Show-LinkTarget.format.ps1xml'))
-  Update-TypeData -PrependPath ([System.IO.Path]::Combine((Split-Path -Path $PSScriptRoot -Parent),'Lib\FileLength-Formatting\MyTypes.types.ps1xml'))
+
+Update-FormatData -AppendPath ([System.IO.Path]::Combine((Split-Path -Path $PSScriptRoot -Parent),'Lib\Formatting\Show-LinkTarget.format.ps1xml'))
+Update-TypeData -AppendPath ([System.IO.Path]::Combine((Split-Path -Path $PSScriptRoot -Parent),'Lib\Formatting\MyTypes.types.ps1xml'))
 
 }
 
--- WozTools.psd1
+++ WozTools.psd1
@@ -60,10 +60,10 @@ PowerShellVersion = '5.1'
 # ScriptsToProcess = @()
 
 # Type files (.ps1xml) to be loaded when importing this module
-TypesToProcess = 'Lib\FileLength-Formatting\MyTypes.types.ps1xml'
+TypesToProcess = 'Lib\Formatting\MyTypes.types.ps1xml'
 
 # Format files (.ps1xml) to be loaded when importing this module
-FormatsToProcess = 'Lib\FileLength-Formatting\Show-LinkTarget.format.ps1xml'
+FormatsToProcess = 'Lib\Formatting\Show-LinkTarget.format.ps1xml'
 
 # Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
 # NestedModules = @()
