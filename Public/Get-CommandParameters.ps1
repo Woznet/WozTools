bd8675a569cde4e2ddc46182a9c20560a4e9f4ea
Woz <walker.chris87@gmail.com>
2022-11-16 05:30:29 -0500

    Update Get-CommandParameters.ps1

--- Public/Get-CommandParameters.ps1
+++ Public/Get-CommandParameters.ps1
@@ -1,4 +1,9 @@
 Function Get-CommandParameters {
+	<#
+	To Do: 
+	Output formating
+	Include command name in output
+	#>
   Param(
     [Parameter(Mandatory)]
     [ValidateScript({
@@ -15,5 +20,5 @@ Function Get-CommandParameters {
   )
 
   (Get-Command -Name $Command).ParameterSets.Parameters | Where-Object {$_.Name -notin $CPara} |
-  Select-Object -Property Name,IsMandatory,Value*,Aliases -Unique | Format-Table -RepeatHeader
+  Select-Object -Property Name,@{n='ParameterType';e={$_.ParameterType.Name}},IsMandatory,Value*,@{n='Aliases';e={$_.Aliases -join ','}} -Unique | Format-Table -RepeatHeader
 }
