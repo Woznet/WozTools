ce20aaa87c9da145d1d35c03f6a3cc508b617160
Woz <walker.chris87@gmail.com>
2023-02-24 20:42:33 -0500

    Update Get-RedirectedUrl.ps1

--- Public/Get-RedirectedUrl.ps1
+++ Public/Get-RedirectedUrl.ps1
@@ -3,7 +3,7 @@ Function Get-RedirectedUrl {
   Param(
     [Parameter(Mandatory,ValueFromPipeline)]
     [ValidateScript({
-          if (-not ([System.Uri]::IsWellFormedUriString($_,[System.UriKind]::Absolute))) {
+          if (-not ([uri]::IsWellFormedUriString($_,[System.UriKind]::Absolute))) {
             throw ('{0}{1} - Failed URL Validation' -f "`n",$_)
           }
           return $true
@@ -11,17 +11,48 @@ Function Get-RedirectedUrl {
     [String]$Url
   )
   begin {
-    $RUrls = [System.Collections.Generic.List[string]]::new()
+
+    Add-Type -AssemblyName System.Net.Http -ErrorAction Stop
+    $HttpClient = [System.Net.Http.HttpClient]::new()
   }
   process {
-    $Request = [System.Net.WebRequest]::Create($Url)
-    $Response = $Request.GetResponse()
-    $RUrls.Add($Response.ResponseUri.ToString())
-    $Response.Close()
-    $Response.Dispose()
+
+    try{
+
+      $HttpRequestMessage = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Head, $Url)
+      $ClientResult = $HttpClient.SendAsync($HttpRequestMessage).GetAwaiter().GetResult().EnsureSuccessStatusCode()
+
+      $ClientResult.RequestMessage.RequestUri.AbsoluteUri                            
+    }
+    catch {
+      [System.Management.Automation.ErrorRecord]$e = $_
+      [PSCustomObject]@{
+        Type      = $e.Exception.GetType().FullName
+        Exception = $e.Exception.Message
+        Reason    = $e.CategoryInfo.Reason
+        Target    = $e.CategoryInfo.TargetName
+        Script    = $e.InvocationInfo.ScriptName
+        Line      = $e.InvocationInfo.ScriptLineNumber
+        Column    = $e.InvocationInfo.OffsetInLine
+      }
+      throw $e
+    }
+
+    <#
+        $Request = [System.Net.WebRequest]::Create($Url)
+        $Response = $Request.GetResponse()
+        $RUrls.Add($Response.ResponseUri.ToString())
+        $Response.Close()
+        $Response.Dispose()
+    #>
+    
   }
   end {
-    return $RUrls
+    $HttpClient.Dispose()
+    $HttpRequestMessage.Dispose()
+    $ClientResult.Dispose()
+    [gc]::Collect()
+    
   }
 }
 
