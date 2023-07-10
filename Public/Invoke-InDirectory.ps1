08ed0552b3ad7776ed353c1b0c7a047b37142f04
woz <walker.chris87@gmail.com>
2023-07-10 08:00:38 -0400

    Update Invoke-InDirectory.ps1

--- Public/Invoke-InDirectory.ps1
+++ Public/Invoke-InDirectory.ps1
@@ -1,16 +1,55 @@
 function Invoke-InDirectory {
-  # https://gist.github.com/chriskuech/a32f86ad2609719598b073293d09ca03#file-tryfinally-2-ps1
+  <#
+      .SYNOPSIS
+      Invoke a scriptblock from within one or more directories
+
+      .DESCRIPTION
+      A longer description.
+
+      .PARAMETER Path
+      Path of one or more directories which the scriptblock will be invoked in
+
+      .PARAMETER ScriptBlock
+      Specifies the commands to run
+
+      .INPUTS
+      System.IO.DirectoryInfo
+      System.String
+
+      You can pipe the output from Get-Item,
+      DirectoryInfo object,
+      A string that contains a path
+
+      .OUTPUTS
+      This function will output whatever is returned from the scriptblock each time it is run.
+
+      .EXAMPLE
+      Invoke-InDirectory -Path 'X:\git\WozTools' -ScriptBlock { git fetch --all }
+
+      .EXAMPLE
+      Get-ChildItem -Path 'X:\git\' -Directory  | Invoke-InDirectory  -ScriptBlock { git fetch --all }
+
+      .EXAMPLE
+      'X:\git\WozTools','.\Git Stuff'  | Invoke-InDirectory  -ScriptBlock { git fetch --all }
+
+      .NOTES
+      Original Source:
+      https://gist.github.com/chriskuech/a32f86ad2609719598b073293d09ca03#file-tryfinally-2-ps1
+  #>
   Param(
-    [Parameter(Mandatory,ValueFromPipeline)]
+    [Parameter(Mandatory,ValueFromPipelineByPropertyName,ValueFromPipeline)]
     [ValidateScript({
           if(-not (Test-Path -Path $_ -PathType Container)) {
             throw 'Folder does not exist'
           }
           return $true
     })]
+    [Alias('FullName')]
+    # Path of one or more directories which the scriptblock will be invoked in
     [String[]]$Path,
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
+    # Specifies the commands to run
     [scriptblock]$ScriptBlock
   )
   process {
