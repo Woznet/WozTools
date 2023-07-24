f0ed8adb117ea9e18e6d93fe881beb97aaef4246
woz <walker.chris87@gmail.com>
2023-07-24 08:04:52 -0400

    Update Get-RedirectedUrl.ps1

--- Public/Get-RedirectedUrl.ps1
+++ Public/Get-RedirectedUrl.ps1
@@ -1,27 +1,59 @@
 Function Get-RedirectedUrl {
+  <#
+      .SYNOPSIS
+      Get the redirected url from the Uri entered in the Uri parameter
+
+      .DESCRIPTION
+      Uses HttpClient and the Head method to reterieve the RequestMessage.RequestUri.AbsoluteUri from the submitted Uri
+
+      .PARAMETER Uri
+      The url that is to be checked for a redirected url
+
+      .INPUTS
+      [string] - Url, accepts multiple Urls
+
+      .OUTPUTS
+      [string] - returns the redirected url
+
+      .EXAMPLE
+      Get-RedirectedUrl https://aka.ms/ad/list
+
+      https://github.com/microsoft/aka#readme
+
+      .EXAMPLE
+      'https://aka.ms/admin',
+      'https://aka.ms/azad',
+      'https://aka.ms/intune',
+      'https://aka.ms/in',
+      'https://aka.ms/entratemplates' | Get-RedirectedUrl
+
+      https://admin.microsoft.com/
+      https://portal.azure.com/Error/UE_404?aspxerrorpath=/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/Overview
+      https://intune.microsoft.com/Error/UE_404?aspxerrorpath=/
+      https://intune.microsoft.com/Error/UE_404?aspxerrorpath=/
+      https://www.microsoft.com/en-us/download/details.aspx?id=57600
+  #>
   Param(
-    [Parameter(Mandatory,ValueFromPipeline)]
+    [Parameter(Mandatory,ValueFromPipeline,Position = 0)]
     [ValidateScript({
           if (-not ([uri]::IsWellFormedUriString($_,[System.UriKind]::Absolute))) {
-            throw ('{0}{1} - Failed URL Validation' -f "`n",$_)
+            throw ('{0}{1} - An invalid request URI was provided. Either the request URI must be an absolute URI or BaseAddress must be set.' -f "`n",$_)
           }
           return $true
     })]
-    [String]$Url
+    # Uri to get the redirected uri
+    [string]$Uri
   )
   begin {
-
     Add-Type -AssemblyName System.Net.Http -ErrorAction Stop
     $HttpClient = [System.Net.Http.HttpClient]::new()
+
+    $Awaiter = @()
   }
   process {
-
     try{
-
-      $HttpRequestMessage = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Head, $Url)
-      $ClientResult = $HttpClient.SendAsync($HttpRequestMessage).GetAwaiter().GetResult().EnsureSuccessStatusCode()
-
-      $ClientResult.RequestMessage.RequestUri.AbsoluteUri
+      $HttpRequestMessage = [System.Net.Http.HttpRequestMessage]::new([System.Net.Http.HttpMethod]::Head, $Uri)
+      $Awaiter += $HttpClient.SendAsync($HttpRequestMessage).GetAwaiter()
     }
     catch {
       [System.Management.Automation.ErrorRecord]$e = $_
@@ -33,15 +65,19 @@ Function Get-RedirectedUrl {
         Script    = $e.InvocationInfo.ScriptName
         Message   = $e.InvocationInfo.PositionMessage
       }
-      throw $e
     }
   }
   end {
+
+    $ClientResultAll = $Awaiter.GetResult()
+    $AbsoluteUri = $ClientResultAll.RequestMessage.RequestUri.AbsoluteUri
+
     $HttpClient.Dispose()
     $HttpRequestMessage.Dispose()
-    $ClientResult.Dispose()
+    $ClientResultAll.Dispose()
     [gc]::Collect()
 
+		return $AbsoluteUri
   }
 }
 
