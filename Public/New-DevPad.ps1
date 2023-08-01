24f3d2dce26e3197b0843b28f6f9e236887b5d8d
woz <walker.chris87@gmail.com>
2023-07-31 21:19:51 -0400

    Update New-DevPad.ps1

--- Public/New-DevPad.ps1
+++ Public/New-DevPad.ps1
@@ -3,7 +3,7 @@ function New-DevPad {
   param(
     [Parameter(Position = 0)]
     [ValidateScript({
-          if(-not (Test-Path -Path $_ -PathType Container -IsValid)){
+          if (-not (Test-Path -Path $_ -PathType Container -IsValid)) {
             throw 'Folder path is not valid.'
           }
           return $true
@@ -13,20 +13,22 @@ function New-DevPad {
     # Do not set the DevPad env variable
     [switch]$NoEnvVariable
   )
-  $DevPad = [System.IO.Path]::Combine($Path,[datetime]::Now.Year,[datetime]::Now.ToString('MM.dd'))
-
+  $DevPad = [System.IO.Path]::Combine($Path, [datetime]::Now.Year, [datetime]::Now.ToString('MM.dd'))
   Write-Verbose -Message ('Creating DevPad folder if it does not exist and set the current working location - {0}' -f $Path)
   New-Item -ItemType Directory -Force -Path $DevPad | Set-Location
-
-  if (-not ($NoEnvVariable)) {
-
-    # $EVDevPad = [environment]::GetEnvironmentVariable('DevPad',[EnvironmentVariableTarget]::Machine)
-    if ($env:DevPad -ne $DevPad) {
-      Write-Verbose -Message ('Settings env variable DevPad to - {0}' -f $Path)
-      [environment]::SetEnvironmentVariable('DevPad',$DevPad,[EnvironmentVariableTarget]::Machine)
-    }
-    else {
-      Write-Verbose -Message 'Skipping SetEnvironmentVariable'
+  if ( -not ($NoEnvVariable)) {
+    $IsAdmin = Test-IfAdmin
+    if ($IsAdmin) {
+      $RegHive = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64)
+      $RegKey = $RegHive.OpenSubKey('System\CurrentControlSet\Control\Session Manager\Environment', $true)
+      $RegKey.SetValue('DevPad', $DevPad)
+      if ($RegKey.GetValue('DevPad') -eq $DevPad) {
+        Write-Verbose -Message 'env:DevPad already set'
+      }
+      else {
+        Write-Verbose -Message ('Set env:DevPad - {0}' -f $Path)
+        $RegKey.SetValue('DevPad', $DevPad)
+      }
     }
   }
 }
