e467316104d6e7c30c5c900fc1cf6af51c92b0a5
woz <walker.chris87@gmail.com>
2023-12-25 03:01:23 -0500

    Update Invoke-InDirectory.ps1

--- Public/Invoke-InDirectory.ps1
+++ Public/Invoke-InDirectory.ps1
@@ -1,35 +1,39 @@
 function Invoke-InDirectory {
     <#
 .SYNOPSIS
-Executes a script block within the context of specified directories.
+Executes a script block within the context of specified directories.  If a file is specified the script block is run in the directory of that file.
 
 .DESCRIPTION
-The Invoke-InDirectory function is designed to execute a provided script block in one or more specified directories. This functionality is particularly useful for commands that are dependent on the current directory context, such as file system operations, git commands, or running scripts that use relative paths.
+The Invoke-InDirectory function is designed to execute a provided script block in one or more specified directories.
+This functionality is particularly useful for commands that are dependent on the current directory context, such as file system operations, git commands, or running scripts that use relative paths.
 
 .PARAMETER Path
-Specifies one or more directories in which the script block will be executed. The function changes to each directory and executes the script block. This parameter accepts pipeline input and can be specified by property name. A validation script ensures that each path provided is an existing directory.
+Specifies one or more paths in which the script block will be executed. When a file is specified the parent directory is used.
+The function changes to each directory and executes the script block. This parameter accepts pipeline input and can be specified by property name.
+A validation script ensures that each path provided exists.
 
 .PARAMETER ScriptBlock
-Defines the script block that will be executed in the context of each specified directory. The script block should contain the commands that you want to run. This parameter is mandatory and cannot be empty.
+Defines the script block that will be executed in the context of each specified directory.
+The script block should contain the commands that you want to run. This parameter is mandatory and cannot be empty.
 
 .EXAMPLE
 Invoke-InDirectory -Path 'C:\Projects\Project1' -ScriptBlock { Get-ChildItem }
 
-This example executes 'Get-ChildItem' in the 'C:\Projects\Project1' directory.
+# This example executes 'Get-ChildItem' in the 'C:\Projects\Project1' directory.
 
 .EXAMPLE
 Get-ChildItem -Path 'C:\Projects\' -Directory | Invoke-InDirectory -ScriptBlock { git status }
 
-This example fetches all directories under 'C:\Projects\' and runs 'git status' in each of them.
+# This example fetches all directories under 'C:\Projects\' and runs 'git status' in each of them.
 
 .EXAMPLE
 'C:\Projects\Project1', 'C:\Projects\Project2' | Invoke-InDirectory -ScriptBlock { git pull }
 
-This example demonstrates using the function with pipeline input to execute 'git pull' in both 'C:\Projects\Project1' and 'C:\Projects\Project2'.
+# This example demonstrates using the function with pipeline input to execute 'git pull' in both 'C:\Projects\Project1' and 'C:\Projects\Project2'.
 
 .INPUTS
 System.String, System.IO.DirectoryInfo
-You can pipe string paths or directory objects to Invoke-InDirectory.
+You can pipe string paths, file objects, or directory objects to Invoke-InDirectory.
 
 .OUTPUTS
 Depends on the script block's output. The function outputs whatever the script block returns for each directory.
@@ -43,13 +47,13 @@ https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everythin
     param(
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, ValueFromPipeline)]
         [ValidateScript({
-                if (-not (Test-Path -Path $_ -PathType Container)) {
-                    throw 'Folder does not exist'
+                if (-not (Test-Path -Path $_)) {
+                    throw 'Path does not exist'
                 }
                 return $true
             })]
         [Alias('FullName')]
-        # Specifies one or more directories in which the script block will be executed.
+        # Specifies one or more paths in which the script block will be executed.
         [String[]]$Path,
 
         [Parameter(Mandatory)]
@@ -59,6 +63,7 @@ https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everythin
     )
     process {
         foreach ($Loc in $Path) {
+            if (Test-Path -Path $Loc -PathType Leaf) { $Loc = Split-Path -Path $Loc -Parent }
             try {
                 Push-Location -Path $Loc
                 . $ScriptBlock
