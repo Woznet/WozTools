f0425d8cbcb8dea1fc10250e98e9a4a9b610a58d
Woz <walker.chris87@gmail.com>
2024-02-19 16:55:33 -0500

    2-19-24

--- Public/Convert-FileLength.ps1
+++ Public/Convert-FileLength.ps1
@@ -1,65 +1,64 @@
 function Convert-FileLength {
-  <#
-      .SYNOPSIS
-      Converts a file length to a human readable format.
+    <#
+        .SYNOPSIS
+        Converts a file length to a human readable format.
 
-      .DESCRIPTION
-      Converts a file length to a human readable format.
+        .DESCRIPTION
+        Converts a file length to a human readable format.
 
-      .PARAMETER Length
-      The file length to convert.
+        .PARAMETER Length
+        The file length to convert.
 
-      .EXAMPLE
-      Convert-FileLength -Length 123456789
+        .EXAMPLE
+        Convert-FileLength -Length 123456789
 
-      Converts the file length 123456789 to a human readable format.
+        Converts the file length 123456789 to a human readable format.
 
-      .EXAMPLE
-      Get-ChildItem | Select-Object -ExpandProperty Length | Convert-FileLength
+        .EXAMPLE
+        Get-ChildItem | Select-Object -ExpandProperty Length | Convert-FileLength
 
-      Converts the file lengths of all files in the current directory to a human readable format.
+        Converts the file lengths of all files in the current directory to a human readable format.
 
-      .NOTES
-      Author: Woz
-  #>
-  [CmdletBinding()]
-  [Alias('Convert-Size')]
-  param(
-    [Parameter(
-      Mandatory,
-      ValueFromPipelineByPropertyName,
-      ValueFromPipeline
-    )]
-    [Alias('Size')]
-    [long]$Length
-  )
-  begin {
-    try {
-      $null = [WozDev.LengthFormatter]
-    }
-    catch {
-      Write-Verbose ' ~ Required Type not loaded, starting Add-Type process ~' -Verbose
-      $MemberDef =  @'
+        .NOTES
+        Author: Woz
+#>
+    [CmdletBinding()]
+    [Alias('Convert-Size')]
+    param(
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName,
+            ValueFromPipeline
+        )]
+        [Alias('Size')]
+        [long]$Length
+    )
+    begin {
+        try {
+            $null = [WozDev.LengthConverter]
+        }
+        catch {
+            Write-Verbose 'Loading Type - WozDev.LengthConverter' -Verbose
+            $MemberDef = @'
 [DllImport("Shlwapi.dll", CharSet = CharSet.Auto)]
 public static extern long StrFormatByteSize(long fileSize, System.Text.StringBuilder buffer, int bufferSize);
 '@
-      $LengthConverter = Add-Type -Name LengthConverter -Namespace 'WozDev' -MemberDefinition $MemberDef
+            $null = Add-Type -Name LengthConverter -Namespace 'WozDev' -MemberDefinition $MemberDef
+        }
+        if (-not ($StringBuilder)) { $global:StringBuilder = [System.Text.StringBuilder]::new(1024) }
     }
-
-  }
-  process {
-    if ('WozDev.LengthFormatter' -as [type]) {
-      if (-not ($StringBuilder)) { $global:StringBuilder = [System.Text.StringBuilder]::new(1024) }
-      $null = [WozDev.LengthFormatter]::StrFormatByteSize(
-        $Length,
-        $StringBuilder,
-        $StringBuilder.Capacity
-      )
-      return $StringBuilder.ToString()
+    process {
+        if ('WozDev.LengthConverter' -as [type]) {
+            $null = [WozDev.LengthConverter]::StrFormatByteSize(
+                $Length,
+                $StringBuilder,
+                $StringBuilder.Capacity
+            )
+            $StringBuilder.ToString() | Write-Output
+        }
+        else {
+            # Add ANSI color for missing LengthConverter
+            $Length | Write-Output
+        }
     }
-    else {
-      # Add ANSI color for missing LengthFormatter
-      return $Length
-    }
-  }
 }
