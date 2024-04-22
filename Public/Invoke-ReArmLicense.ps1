14697401afa4c705e0615a1b49177e1509ee68c6
Woz <walker.chris87@gmail.com>
2024-04-22 02:57:35 -0400

    Update Invoke-ReArmLicense.ps1

--- Public/Invoke-ReArmLicense.ps1
+++ Public/Invoke-ReArmLicense.ps1
@@ -18,6 +18,23 @@ function Invoke-ReArmLicense {
     [System.Management.Automation.Credential()]
     [pscredential]$Credential
   )
+
+Add-Type -TypeDefinition @'
+namespace WozDev
+{
+    public enum LicenseStatus
+    {
+        Unlicensed = 0,
+        Licensed = 1,
+        OOBGrace = 2,
+        OOTGrace = 3,
+        NonGenuineGrace = 4,
+        NotActivated = 5,
+        ExtendedGrace = 6
+    }
+}
+'@
+
   $NewCimParams = @{}
   if ($Port) { $NewCimParams.Port = $Port }
   if ($Credential) { $NewCimParams.Credential = $Credential }
